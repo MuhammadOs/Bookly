26a583d1251e61497bd81041eee6793962b77494
Muhammad Osama <mo1318@fayoum.edu.eg>
2024-05-12 15:00:13 +0300

    Bloc cubit - dio - localization - theming

---
+++ lib/config/router/routes.dart
@@ -0,0 +1,7 @@
+class AppRoutes {
+  static const String home = 'home';
+  static const String splash = 'Splash';
+  static const String details = 'details';
+  static const String search = 'search';
+  static const String setting = 'setting';
+}
